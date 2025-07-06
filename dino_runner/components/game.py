a8a938ba0b127ff01541e45dd43dfe84405df81d
victorbitar-mverickx <victorbitar13@gmail.com>
2025-07-06 12:56:15 -0300

    teste

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,3 +1,9 @@
 import pygame   
 
-class Game:
+class Game: 
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((800, 600))
+        pygame.display.set_caption("Dino Runner")
+        self.clock = pygame.time.Clock()
+        self.running = True
