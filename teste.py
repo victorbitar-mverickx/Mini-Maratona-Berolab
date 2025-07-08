f2f3d777728c83b7883942344203973a20f6cb30
victorbitar-mverickx <victorbitar13@gmail.com>
2025-07-08 11:59:37 -0300

    teste

--- teste.py
+++ teste.py
@@ -1,12 +1,11 @@
-from dino_runner.components.game import Game
-import pygame
 from pygame.locals import *
+import pygame
 from sys import exit
 
-pygame.init()
+pygame.init()  
 
-largura = 640 #eixo y
-altura = 480 #eixo x
+largura = 640 
+altura = 480
 
 tela = pygame.display.set_mode((largura, altura)) #CRiAndo a tela
 pygame.display.set_caption("Dinobero")  # Definindo o nome da janela
@@ -16,10 +15,7 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
+
+
+    pygame.draw.rect(tela, (255, 0, 0), (100, 100, 50, 50))  # Desenhando um retângulo vermelho
     pygame.display.update()  # Atualizando a tela  do joguinho
-
-if __name__ == "__main__":
-    game = Game()
-    game.run()
-
-
